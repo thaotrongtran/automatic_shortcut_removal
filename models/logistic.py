018e03478e8e6466ef96f8647c986d7fe6b65d48
thaotrongtran <thaottrong@gmail.com>
2022-04-14 23:43:31 -0400

    Update downstream head

--- models/logistic.py
+++ models/logistic.py
@@ -1,6 +1,19 @@
 import torch
 import torch.nn as nn
 
+class class_head(nn.Module):
+    def __init__(self, in_features, out_features):
+        super().__init__()
+        self.pooling = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        self.fc = nn.Sequential(
+         nn.Flatten(),
+         nn.BatchNorm1d(in_features, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+         nn.Linear(in_features=in_features, out_features=out_features, bias=False))
+    def forward(self, x):
+        x = self.pooling(x)
+        x = self.fc(x)
+        return x
+
 class Logistic_Net(nn.Module):
     def __init__(self, pretrained_extractor_path, pretrained_lens_path = None):
         super().__init__()
@@ -9,23 +22,16 @@ class Logistic_Net(nn.Module):
             self.lens = torch.load(pretrained_lens_path)
             for param in self.lens.parameters():
                 param.requires_grad = False
-        self.extractor = torch.load(pretrained_extractor_path)
+        res = torch.load(pretrained_extractor_path)
+        self.extractor = nn.Sequential(*list(list(res.children())[0].children())[0:-2])
         self.extractor.eval()
         for param in self.extractor.parameters():
             param.requires_grad = False
-        num_channels = 2048
-        num_feats = 1000
-        num_classes = 10
-        self.extractor.res.fc = nn.Sequential(
-            nn.Linear(num_channels, num_feats, bias=False),
-            nn.ReLU(inplace=True),
-            nn.Linear(num_feats, num_feats, bias=False),
-            nn.ReLU(inplace=True),
-            nn.Linear(num_feats, num_classes)
-        )
+        self.head = class_head(in_features=2048, out_features=10)
 
     def forward(self, x):
         if self.lens_usage:
             x = self.lens(x)
         x = self.extractor(x)
+        x = self.head(x)
         return x
